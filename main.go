dbb2e17b327bc2ec500ac86f4d63c05b46b9e61a
Thomas Cooper <thomascooperca@outlook.com>
2020-02-07 22:23:33 -0500

    implemented main policy rule generation logic
    still need to de-dup resources across API groups

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sJson "k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -25,12 +28,63 @@ func main() {
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		panic(err.Error())
+		log.Printf("ERROR! Unable to build a valid Kubernetes config, %s", err.Error())
+		return
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		log.Printf("Error during Kubernetes client initialization, %s", err.Error())
+		return
+	}
+
+	apiResourceListArray, err := clientset.Discovery().ServerResources()
+	if err != nil {
+		log.Printf("Error during server resource discovery, %s", err.Error())
+		return
+	}
+
+	computedPolicyRules := make([]rbacv1.PolicyRule, 0)
+
+	for _, apiResourceList := range apiResourceListArray {
+
+		log.Printf("\n\tAPI Group: %s", apiResourceList.GroupVersion)
+		// rbac rules only look at API group names, not name & version
+		groupOnly := strings.Split(apiResourceList.GroupVersion, "/")[0]
+		// core API doesn't have a group "name". In rbac policy rules, its a blank string
+		if groupOnly == "v1" {
+			groupOnly = ""
+		}
+
+		resourceList := make([]string, 0)
+		uniqueVerbs := make(map[string]bool)
+		for _, apiResource := range apiResourceList.APIResources {
+			log.Printf("\n\tGroup Name: %s\n\tResource: %s\n\tVerbs: %s\n",
+				groupOnly,
+				apiResource.Name,
+				apiResource.Verbs.String())
+
+			resourceList = append(resourceList, apiResource.Name)
+			for _, verb := range apiResource.Verbs {
+				uniqueVerbs[verb] = true
+			}
+		}
+
+		verbList := make([]string, len(uniqueVerbs))
+		i := 0
+		for k := range uniqueVerbs {
+			verbList[i] = k
+			i++
+		}
+
+		newPolicyRule := &rbacv1.PolicyRule{
+			APIGroups: []string{groupOnly},
+			Verbs:     verbList,
+			Resources: resourceList,
+		}
+
+		computedPolicyRules = append(computedPolicyRules, *newPolicyRule)
+
 	}
 
 	completeRbac := &rbacv1.ClusterRole{
@@ -41,31 +95,17 @@ func main() {
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "foo-role",
 		},
+		Rules: computedPolicyRules,
 	}
-	apiResourceListArray, err := clientset.Discovery().ServerResources()
-	if err != nil {
-		panic(err.Error())
-	}
-	for _, apiResourceList := range apiResourceListArray {
-		for _, apiResource := range apiResourceList.APIResources {
-			fmt.Printf("\n\tGroup Name: %s\n\tResource: %s\n\tVerbs: %s\n",
-				apiResourceList.GroupVersion,
-				apiResource.Name,
-				apiResource.Verbs.String())
 
-		}
-	}
 	serializer := k8sJson.NewYAMLSerializer(k8sJson.DefaultMetaFactory, nil, nil)
 	var writer = bytes.NewBufferString("")
 	e := serializer.Encode(completeRbac, writer)
 	if e != nil {
-		panic(e.Error())
+		log.Printf("Error encountered during YAML encoding, %s", e.Error())
+		return
 	}
-	fmt.Printf("\nComplete RBAC object: \n\n%s", writer.String())
-}
-
-func createKey(s []string) string {
-	return fmt.Sprintf("%q", s)
+	fmt.Println(writer.String())
 }
 
 func homeDir() string {
